85ccd732211f70aeffaedfd8e7bf61ced76a770c
Rodrigo Ribeiro <rodrigo.santos@arphoenix.com.br>
2025-04-27 21:44:45 -0300

    change hashRouter

--- src/routers/routers.jsx
+++ src/routers/routers.jsx
@@ -1,21 +1,22 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Register from "../pages/Register";
 import Login from "../pages/Login";
 import EditUser from "../pages/Configs";
 import ForgotPassword from "../pages/ForgotPassword";
 import Influencer from "../pages/Influencer";
+
 const Routers = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/:id/:channelName" element={<Influencer />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/reset-password/:token" element={<ForgotPassword />} />
         <Route path="/configuracoes" element={<EditUser />} />
+        <Route path="/:id/:channelName" element={<Influencer />} />
       </Routes>
     </Router>
   );
