98a595b525e0f81b47ed3d1158bbef0d082bf4cb
Rodrigo Ribeiro <rodrigo.santos@arphoenix.com.br>
2025-04-27 22:04:44 -0300

    testando BrowserRouter

--- src/routers/routers.jsx
+++ src/routers/routers.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Register from "../pages/Register";
 import Login from "../pages/Login";
